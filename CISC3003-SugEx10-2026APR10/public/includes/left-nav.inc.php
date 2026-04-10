83c6752a35e8a8e04f3e720268a761ac39301e97
Jetting-L <dc22892@umac.mo>
2026-04-10 20:14:31 +0800

    Delete CISC3003-SugEx10-2026APR10/public/includes directory

--- CISC3003-SugEx10-2026APR10/public/includes/left-nav.inc.php
+++
@@ -1,9 +0,0 @@
-<div class="profile mdl-color--blue-grey-900 mdl-color-text--blue-grey-50">
-    <img src="images/profile.jpg" class="avatar">
-    <h4>John Locke</h4>
-    <span>johnlocke@example.com</span>
-</div>
-<nav class="mdl-navigation">
-    <a class="mdl-navigation__link" href="">Dashboard</a>
-    <a class="mdl-navigation__link" href="">Customers</a>
-    </nav>
